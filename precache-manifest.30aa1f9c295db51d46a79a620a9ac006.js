b445a3dde55d02ad26625a1f943c13e63b42da15
SvenHolmager <sholmager@gmail.com>
2019-02-15 15:39:21 +0100

    Updates

---
+++ precache-manifest.30aa1f9c295db51d46a79a620a9ac006.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1710f0add136cdba877",
+    "url": "/webpage/static/css/main.a1197de4.chunk.css"
+  },
+  {
+    "revision": "a1710f0add136cdba877",
+    "url": "/webpage/static/js/main.a1710f0a.chunk.js"
+  },
+  {
+    "revision": "fe13aa552d4f1894ca03",
+    "url": "/webpage/static/js/1.fe13aa55.chunk.js"
+  },
+  {
+    "revision": "35c6d35963d4e3bd941d",
+    "url": "/webpage/static/js/runtime~main.35c6d359.js"
+  },
+  {
+    "revision": "ee5c0f77c3bf92af3e6a2fd32e060b82",
+    "url": "/webpage/static/media/Openbot.ee5c0f77.svg"
+  },
+  {
+    "revision": "28ca8de76f69df47bb2579c78188494a",
+    "url": "/webpage/static/media/twitter.28ca8de7.svg"
+  },
+  {
+    "revision": "f10eba06c208b7de4b405483c8c9c6d5",
+    "url": "/webpage/static/media/discord.f10eba06.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/webpage/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "b28063b5263e0f9a2ffd061ffd597ae6",
+    "url": "/webpage/index.html"
+  }
+];
